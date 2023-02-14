ff350aaf82bc697806b0060dbdd9203cbe6218d2
Aiden Mizhen <amizhen30@stuy.edu>
2023-02-14 15:34:01 -0500

    It works, gonna shorten

--- utilities.py
+++ utilities.py
@@ -18,21 +18,68 @@ class Screen:
                 self.screen[row][col] = np.array(d_color)
 
     def line(self, x1, y1, x2, y2, color=(0, 0, 0)):
+        if x1 > x2:  # Make x2 > x1
+            y1, y2 = y2, y1
+            x1, x2 = x2, x1
 
-        # Casework for negative / negative slope
-        if x1 > x2:
-            if y1 > y2:
-                y1, y2 = y2, y1
-                x1, x2 = x2, x1
+        delta = 1
+        if y2 < y1:  # If Y is decreasing
+            delta = -1
+        cost = 0
+        if delta == 1:
+            print(1)
+            if x2 - x1 >= abs(y2 - y1):  # |m| <= 1
+                x, y = x1, y1
+                while x <= x2:  # Inclusive endpoints
+                    self.pixel(x, y, d_color=color)
+                    x += 1
+                    cost += (2 * y2 - 2 * y1)*delta  # Adjust for x
+                    if cost + (- x2 + x1) * delta > 0:  # If under the line
+                        y += delta
+                        cost += (-2 * x2 + 2 * x1)*delta
 
-        x, y = x1, y1
-        while x < x2:
-            self.pixel(x, y, d_color=color)
-            if (y2 - y1) * (x - x1) > (x2 - x1) * (y - y1):  # If under the line
-                y += 1
-            x += 1
-        self.pixel(x1, y1, d_color=(255, 0, 0))
-        self.pixel(x2, y2, d_color=(255, 0, 0))
+            else:
+                # if delta == -1:
+                #     y1, y2 = y2, y1
+                #     x1, x2 = x2, x1
+                x, y = x1, y1
+                # print(2, x1, y1, x2, y2)
+                while y <= y2:  # Inclusive endpoints
+                    self.pixel(x, y, d_color=color)
+                    y += 1
+                    cost += (-2 * x2 + 2 * x1)*delta  # Adjust for y
+                    if cost + (y2 - y1)*delta < 0:  # If over the line
+                        x += delta
+                        cost += (2 * y2 - 2 * y1)*delta
+        else:
+            print(2)
+            if x2 - x1 >= abs(y2 - y1):  # |m| <= 1
+                x, y = x1, y1
+                while x <= x2:  # Inclusive endpoints
+                    self.pixel(x, y, d_color=color)
+                    x += 1
+                    cost += 2 * y2 - 2 * y1  # Adjust for x
+                    if cost + (- x2 + x1) * delta < 0:  # If under the line
+                        y += delta
+                        cost += (-2 * x2 + 2 * x1) * delta
+
+            else:
+                if delta == -1:
+                    y1, y2 = y2, y1
+                    x1, x2 = x2, x1
+                x, y = x1, y1
+                # print(2, x1, y1, x2, y2)
+                while y <= y2:  # Inclusive endpoints
+                    self.pixel(x, y, d_color=color)
+                    y += 1
+                    cost += -2 * x2 + 2 * x1  # Adjust for y
+                    if cost + (y2 - y1) * delta > 0:  # If over the line
+                        x += delta
+                        cost += (2 * y2 - 2 * y1) * delta
+        # 249, 249, 0, 499
+        # 0 499 249 249
+        # self.pixel(x1, y1, d_color=(255,0,00))
+        # self.pixel(x2, y2, d_color=(255,0,00))
 
     # Utility Functions
 
@@ -59,11 +106,21 @@ class Screen:
         self.img.putdata(pixels)
         self.img.show()
 
+if __name__ =='main':
+    s = Screen(500, 500)
+    s.line(249, 249, 499, 249, color=(255, 0, 0))  # m = 0
+    s.line(249, 249, 499, 499, color=(0, 255, 0))  # m = 1
+    s.line(249, 249, 249, 499, color=(0, 0, 255))  # m = infinity
+    s.line(249, 249, 0, 499, color=(255, 255, 0))  # m = -1
+    # s.line(249, 249, 0, 249, color=(255, 0, 255))  # m = -0
+    # s.line(249, 249, 0, 0, color=(0, 255, 255))  # m = 1
+    # # s.line(249, 249, 349, 0, color=(0, 128, 128))  # m = -infinity
+    # # s.pixel(349,0,(255,255,255))
+    # s.line(249, 249, 499, 0, color=(128, 128, 0))  # m = -1
 
-s = Screen(500, 500)
-# s.line(100, 10, 270, 100, color=(0, 0, 255))
-s.line(270, 100, 100, 10, color=(0, 0, 255))
-# s.color_screen((0,255,0))
-s.display()
-# s.write_ppm("test.ppm")
-# s.save_extension("test.png")
+
+    s.pixel(249, 249, d_color=(255, 255, 255))
+
+    s.display()
+    # s.write_ppm("test.ppm")
+    # s.save_extension("test.png")
